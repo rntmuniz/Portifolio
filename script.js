4e0ca4043ffe956614203cd051e20a8b0802274d
Renato Muniz <rntmuniz@gmail.com>
2024-07-23 18:48:22 -0300

    corrige titulos modal

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 let buttonsList = [
     {'icon':'bx bxs-face', 'title': 'Perfil', 'body': 'Sou um profissional que já atua no desenvolvimento de software há 12 anos tendo uma vasta experiência em desenvolvimento mainframe atuando principalmente em empresa do sistema fianceiro nacional.'},
-    {'icon':'bx bxs-pencil', 'title': 'Formacao', 'body': '* Lienciatura em Ciências da Computação - UESPI (2006); <br>* Pós Graduação Análise de Sistemas - FAP (2007); <br>* Técnico em Informática - CEFET PI (2008); <br>* Pós Gradução Análise Forense e Perícia Digital - IPOG (2019); <br>* MBA Cybersegurança - FIAP (2023); <br>* Certificação em CyberSegurança - IBSEC (Junho/2024);'},
-    {'icon':'bx bxs-landmark', 'title': 'Experiencia', 'body': '* Técnico em Informática - Prefeitura Municipal de Parnaíba (7 anos); <br>* Professor Efetivo de Computação - SEDUC PI (1 ano); <br>* Assessor de TI - Banco do Brasil (11/13 anos);'},
+    {'icon':'bx bxs-pencil', 'title': 'Formação', 'body': '* Lienciatura em Ciências da Computação - UESPI (2006); <br>* Pós Graduação Análise de Sistemas - FAP (2007); <br>* Técnico em Informática - CEFET PI (2008); <br>* Pós Gradução Análise Forense e Perícia Digital - IPOG (2019); <br>* MBA Cybersegurança - FIAP (2023); <br>* Certificação em CyberSegurança - IBSEC (Junho/2024);'},
+    {'icon':'bx bxs-landmark', 'title': 'Experiência', 'body': '* Técnico em Informática - Prefeitura Municipal de Parnaíba (7 anos); <br>* Professor Efetivo de Computação - SEDUC PI (1 ano); <br>* Assessor de TI - Banco do Brasil (11/13 anos);'},
     {'icon':'bx bxs-message-alt-detail', 'title': 'Depoimento', 'body': '<i>Você vai evoluir a medida aprender algo novo além daquilo que já domina.'},
     {'icon':'bx  bx-laptop', 'title': 'Trabalhos', 'body': 'Projetos Internos na Diretoria de Tecnologia do Banco do Brasil na área de Desenvolvimento de Software Plataforma Mainframe, Java, JavaCloud, entre outros.'},
     {'icon':'bx bx-toggle-right', 'title': 'Programe-IO', 'body': 'link'},
@@ -26,7 +26,7 @@ document.getElementById('button-list').innerHTML = content;
 
 function abrirModal(title, body){
     let link = `<a href="https://rntmuniz.github.io/CalculadoraHtml/">Calculadora HTML</a><br> 
-    <a href="https://www.google.com">Calculadora Plus HTML</a>`;
+    <a href="https://rntmuniz.github.io/CalculadoraHtml/">Calculadora Plus HTML</a>`;
 
     // const texto = document.querySelector('.texto');
     const modal = document.getElementById('janela-modal');
